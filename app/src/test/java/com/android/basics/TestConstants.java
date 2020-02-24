46d0bd47a08b92197d447b406d8acf1c715c15d7
kalyankumar <kalyand14@gmail.com>
2020-02-24 16:08:01 +0530

    Added testcases for AuthenticateUserInteractor

---
+++ app/src/test/java/com/android/basics/TestConstants.java
@@ -0,0 +1,6 @@
+package com.android.basics;
+
+public class ToDoApplicationTestConstants {
+    public static final String USER_NAME = "Kalyan";
+    public static final String PASSWORD = "password";
+}
